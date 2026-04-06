c42990ace9b6c45edcc74844e0194651853c361d
CamHa05 <camha20111@gmail.com>
2026-04-06 22:45:18 +0700

    Khi vào trang chủ, mặc định sẽ hiển thị 12 bộ phim có độ phổ biến (popularity) lớn hơn 450 và có điểm bình chọn trung bình (vote_average) lớn hơn 7, sắp xếp giảm dần theo ngày phát hành (release_date).

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MovieController extends Controller
 {
     //
     public function index(){
-        return view("movie.index");
+        $movie = DB::select("SELECT * FROM movie WHERE popularity > 450 AND vote_average > 7 ORDER BY release_date DESC LIMIT 12;");
+        return view("movie.index",compact("movie"));
     }
 }
--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -2,4 +2,13 @@
     <x-slot name="title">
         Movie
     </x-slot>
+    <div class="list-movie">
+    @foreach($movie as $row)
+        <div class="movie">
+            <img src="{{asset('storage/'.$row->image)}}" alt="" style="width: 100%; height: 300px; object-fit: cover;">
+            <b>{{$row->movie_name_vn}}</b><br>
+            {{$row->release_date}}
+        </div>    
+    @endforeach
+    </div>
 </x-movie-layout>
