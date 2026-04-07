3cce278b963f81a4d91af8471c9435f93837473f
Bear531 <luutrinhgiayen2@gmail.com>
2026-04-07 10:42:11 +0700

    quản lý phim

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController2;
 
 Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
+Route::get('/movielist', [MovieController2::class, 'movieList'])->name('movielist');
+Route::get('/moviecreate', [MovieController2::class, 'movieCreate'])->name('moviecreate');
+Route::get('/moviedelete', [MovieController2::class, 'movieDelete'])->name('moviedelete');
+Route::post('/moviedelete', [MovieController2::class, 'movieDelete'])->name('moviedelete');
+Route::get('/moviesave', [MovieController2::class, 'movieSave'])->name('moviesave');
+Route::post('/moviesave', [MovieController2::class, 'movieSave'])->name('moviesave');
